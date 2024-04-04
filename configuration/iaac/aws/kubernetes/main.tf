5d006655727ef69036fd4959aa1f98e7f7594a23
Khumaini Shaik <92944411+KhumainiShaik@users.noreply.github.com>
2024-04-04 22:36:53 +0530

    Update main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -1,8 +1,8 @@
 # aws --version
 # aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
-# terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
+#arn:aws:s3:::terraform-backend-state-khumaini
+#AKIAYK7TLQWPFM4QOMU4
 
 
 terraform {
@@ -25,20 +25,20 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  version                = "~> 2.12"
+  #version                = "~> 2.12"
 }
 
-module "in28minutes-cluster" {
+module "khumaini-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
+  cluster_name    = "khumaini-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
+  subnet_ids      = ["subnet-01c33deda6595001a", "subnet-0698a7795b67738aa"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
   #vpc_id         = "vpc-1234556abcdef"
 
-  node_groups = [
+  eks_managed_node_groups = [
     {
       instance_type = "t2.micro"
       max_capacity  = 5
@@ -49,11 +49,11 @@ module "in28minutes-cluster" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.khumaini-cluster.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
+  name = module.khumaini-cluster.cluster_id
 }
 
 
